9f2065b2094283c5a5ccfcf8253100bc737eb56a
proudparrot2 <71939973+proudparrot2@users.noreply.github.com>
2023-01-13 09:28:28 -0600

    Added edit bookmarklet

---
+++ edit.js
@@ -0,0 +1,9 @@
+/* 
+Editing bookmarklet
+
+Allows you to edit
+the text of a site 
+as if it is a textbox
+*/
+
+javascript:document.body.contentEditable = 'true'; document.designMode='on'; void 0;
